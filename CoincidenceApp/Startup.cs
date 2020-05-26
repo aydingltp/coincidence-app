eebf101f2a0a9e3552960a67bc044186669b0923
aydingltp <aydngltp@gmail.com>
2020-05-26 11:49:49 +0300

    thema added

--- CoincidenceApp/Startup.cs
+++ CoincidenceApp/Startup.cs
@@ -18,58 +18,65 @@ using VueCliMiddleware;
 
 namespace CoincidenceApp
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-          services.AddSpaStaticFiles(opt => opt.RootPath = "ClientApp/dist");
-            services.AddControllers();
-            services.AddDbContext<DataContext>();
-              //  options.UseSqlServer(Configuration.GetConnectionString("DataContext")));
-               
-                 
-            services.AddSingleton<IHostedService, TimedHostedService>();
-            services.AddHostedService<TimedHostedService>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            // app.UseEndpoints(endpoints =>
-            // {
-            //     endpoints.MapControllers();
-            // });
-            app.UseEndpoints(endpoints =>
-            {
-              endpoints.MapControllers();
-
-              endpoints.MapToVueCliProxy(
-                "{*path}",
-                new SpaOptions { SourcePath = "ClientApp" },
-                npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
-                regex: "Compiled successfully",
-                forceKill: true
-              );
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>();
+      //  options.UseSqlServer(Configuration.GetConnectionString("DataContext")));
+
+
+      services.AddSingleton<IHostedService, TimedHostedService>();
+      services.AddHostedService<TimedHostedService>();
+        services.AddControllers();
+      services.AddRazorPages();
+      services.AddSpaStaticFiles(opt => opt.RootPath = "ClientApp/dist");
+      
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+      app.UseSpaStaticFiles();
+      // app.UseEndpoints(endpoints =>
+      // {
+      //     endpoints.MapControllers();
+      // });
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+
+        if (env.IsDevelopment())
+        {
+          endpoints.MapToVueCliProxy(
+            "{*path}",
+            new SpaOptions {SourcePath = "ClientApp"},
+            npmScript: (System.Diagnostics.Debugger.IsAttached) ? "dev" : null,
+            regex: "Compiled successfully",
+            forceKill: true,
+            port: 8080
+          );
+        }
+      });
+      app.UseSpa(spa => { spa.Options.SourcePath = "ClientApp"; });
+    }
+  }
 }
