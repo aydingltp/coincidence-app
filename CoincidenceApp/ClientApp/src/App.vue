3a3c73958aee97030e4ff2a86890da32749470ed
aydingltp <aydngltp@gmail.com>
2020-06-05 12:43:26 +0300

    thema deleted

--- CoincidenceApp/ClientApp/src/App.vue
+++ CoincidenceApp/ClientApp/src/App.vue
@@ -1,55 +1,114 @@
 <template>
   <div id="app">
-    <app-header></app-header>
-    <router-view></router-view>
-    <app-footer></app-footer>
+    <div id="particles-js">
+      <div class="panel" style="display: block">
+        <h1 class="baslik">TESADÜF SAYACI</h1>
+
+        <h4>Aranan Cümle : Tesadüf Anlamlı Şeyler Yaratabilir.</h4>
+        <div>
+          <h4>{{jsondata[1]}}</h4> <h4>Deneme : {{jsondata[0]}} </h4>
+        </div>
+
+        <div class="row zaman">
+          <div class="col-sm-2">
+            <div class="row round">
+              {{jsondata[5]}}
+            </div>
+            <div class="row round">
+              <h5><span class="badge badge-info badge-pill">Gün</span></h5>
+            </div>
+          </div>
+          <div class="col tire">:</div>
+          <div class="col-sm-2">
+            <div class="row round">{{jsondata[4]}}</div>
+            <div class="row round">
+              <h5><span class="badge badge-info badge-pill">Saat</span></h5>
+            </div>
+          </div>
+          <div class="col tire">:</div>
+          <div class="col-sm-2">
+            <div class="row round">{{jsondata[3]}}</div>
+            <div class="row round">
+              <h5><span class="badge badge-info badge-pill">Dakika</span></h5>
+            </div>
+          </div>
+          <div class="col tire">:</div>
+          <div class="col-sm-2">
+            <div class="row round">{{jsondata[2]}}</div>
+            <div class="row round">
+              <h5><span class="badge badge-info badge-pill">Saniye</span></h5>
+            </div>
+          </div>
+          <div class="row description">
+            <small>Eski zamanlarda yaşayan bir Tesadüf Tanrısı vardı. En büyük gücü olan zamanla olağanüstü şeyler
+              yaratırdı. Öyle ki insanların bile yapamadığı şeyleri o kolaylıkla yapabiliyordu. Mikrodan makroya gözümüzün
+              gördüğü bütün güzellikleri yaratabilecek bir kudreti vardı. O olağanüstü mükemmel tanrının tek bir eksikliği,
+              noksanlığı vardı : ZAMAN...
+            </small>
+            <small
+              style="margin-top: 5px; margin-left: 35%; color:rgba(255,255,255,0.32); font-family: 'Comic Sans MS',monospace">
+              &copy; made by lâvehn - Source on :
+              <a href="https://github.com/aydingltp/coincidence-app" style="color: rgba(255,255,255,0.32)">Github</a>
+            </small>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+<!--      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"-->
+<!--              integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"-->
+<!--              crossorigin="anonymous"></script>-->
+<!--      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"-->
+<!--              integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"-->
+<!--              crossorigin="anonymous"></script>-->
+<!--      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"-->
+<!--              integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"-->
+<!--              crossorigin="anonymous"></script>-->
+
+
+    </div>
   </div>
 </template>
 
+
 <script>
-  import Header from "./components/shared/Header";
-  import Home from "./components/Home";
-  import Footer from "./components/shared/Footer";
+  import axios from 'axios';
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      data1: null,
+      url: "https://localhost:5001/api/tesaduf",
+      jsondata: []
+    }
+  },
+  methods: {
+    getData() {
+      this.data1 = setInterval(() => {
+        axios.get(this.url, {}).then(obj => {
+          this.jsondata = obj.data;
+          //console.log(obj.data);
+        })
+      }, 1000)
     }
   },
   components: {
-      appHeader: Header,
-      appHome: Home,
-      appFooter: Footer
+  },
+  created() {
+    this.getData()
   }
 }
 </script>
+<!-- particles.js lib (JavaScript CodePen settings): https://github.com/VincentGarreau/particles.js -->
+
+
+<!--===============================================================================================-->
 
 <style>
 #app {
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
+
 }
-
-/*h1, h2 {*/
-/*  font-weight: normal;*/
-/*}*/
-
-/*ul {*/
-/*  list-style-type: none;*/
-/*  padding: 0;*/
-/*}*/
-
-/*li {*/
-/*  display: inline-block;*/
-/*  margin: 0 10px;*/
-/*}*/
-
-/*a {*/
-/*  color: #42b983;*/
-/*}*/
 </style>
--- CoincidenceApp/ClientApp/src/main.js
+++ CoincidenceApp/ClientApp/src/main.js
@@ -1,14 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import {router} from "./router";
-//import VueSocketIO from 'vue-socket.io';
-//import socketio from 'socket.io';
-
-//export const SocketInstance = 'https://localhost:5001/api/tesaduf';
-//
-// Vue.use(new VueSocketIO({
-//   connection:'https://localhost:5001/api/tesaduf'
-// }))
 
 Vue.filter("zaman", (value) => {
   return parseFloat(value).toLocaleString(undefined, {minimumIntegerDigits : 2})
