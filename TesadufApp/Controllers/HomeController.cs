d76adde766f292394cf0f6c58267f8a982f39729
aydingltp <aydngltp@gmail.com>
2021-09-21 22:24:00 +0300

    home update

--- TesadufApp/Controllers/HomeController.cs
+++ TesadufApp/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using TesadufApp.Data;
 using TesadufApp.Models;
+using TesadufApp.Models.ViewModel;
 
 namespace TesadufApp.Controllers
 {
@@ -15,11 +11,8 @@ namespace TesadufApp.Controllers
     {
         private readonly DataContext _db;
 
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger, DataContext db)
+        public HomeController(DataContext db)
         {
-            _logger = logger;
             _db = db;
         }
 
@@ -32,50 +25,29 @@ namespace TesadufApp.Controllers
         {
             return View();
         }
-
-        public string GetData()
+        
+        public IActionResult GetDatas()
         {
-            var entity = _db.SonDegerler.OrderBy(p => p.Id).FirstOrDefault();
-            var zaman = _db.Zamanlar.OrderBy(p => p.Id).FirstOrDefault();
-            if (entity != null && zaman != null)
-            {
-                var json = new List<string>
-                {
-                    entity.Sayac.ToString(String.Format("000 000 000 000")),
-                    entity.GelenDeger,
-                    zaman.Saniye.ToString(String.Format("00")),
-                    zaman.Dakika.ToString(String.Format("00")),
-                    zaman.Saat.ToString(String.Format("00")),
-                    zaman.Gun.ToString(String.Format("00"))
-                };
-
-                return JsonConvert.SerializeObject(json);
-
-            }
-            return null;
-        }
-        public string GetDatas()
-        {
-            var entity = _db.SonDegerlerAlls.OrderByDescending(p => p.Id).Take(300).ToList();
+            var entity = _db.SonDegerlerAlls.OrderByDescending(p => p.Id).Take(600).ToList();
             entity.Reverse();
-            var json = new List<JsonData>();
-            foreach (var item in entity)
+
+            var result = JsonHelper(entity);
+            
+            return Json(result);
+        }
+
+        [NonAction]
+        public List<JsonData> JsonHelper(List<SonDegerlerAll> data)
+        {
+            return data.Select(item => new JsonData
             {
-                json.Add(new JsonData
-                {
-                    GelenDeger = item.GelenDeger,
-                    Sayac = item.Sayac.ToString(String.Format("000 000 000 000")),
-                    Saniye = item.Saniye.ToString(String.Format("00")),
-                    Dakika = item.Dakika.ToString(String.Format("00")),
-                    Saat = item.Saat.ToString(String.Format("00")),
-                    Gun = item.Gun.ToString(String.Format("00"))
-                });
-            }
-            if (entity != null)
-            {
-                return JsonConvert.SerializeObject(json);
-            }
-            return null;
+                GelenDeger = item.GelenDeger,
+                Sayac = item.Sayac.ToString("000 000 000 000"),
+                Saniye = item.Saniye.ToString("00"),
+                Dakika = item.Dakika.ToString("00"),
+                Saat = item.Saat.ToString("00"),
+                Gun = item.Gun.ToString("00")
+            }).ToList();
         }
     }
 }
